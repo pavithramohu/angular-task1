3d8166dbb98a58db7d04697809e34babfd6ce52c
Pavithra Mohanbabu <pavithra.m@firstqa.com>
2022-01-18 23:09:07 +0530

    code organized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/services/book.service';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,22 +12,17 @@ export class AppComponent implements OnInit {
   value = false;
   showSpinner = false;
   errorMessage: string = "";
+
   constructor(private bookService: BookService) {}
-
-  title = 'task';
   ngOnInit() {}
-
   show() {
     this.showSpinner = true;
-    console.log(this.bookName);
     this.bookService.getBooksDetails(this.bookName).subscribe((d) => {
-      //.log(d.docs[0]);
         this.bookDetails = d.docs[0];
-        console.log(this.bookDetails);
         this.value = true;
         this.showSpinner = false;
     }, error => {
-      this.errorMessage = " No data found";
+      this.errorMessage = "No data found";
       this.value = false;
       this.showSpinner = false;
     });
--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,20 +1,14 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
-    constructor(
-        private http: HttpClient
-    ) { }
+  constructor(private http: HttpClient) {}
 
-    getBooksDetails(name: string): Observable<any> {
-      //  const a = this.http.get<any>('//openlibrary.org/books/OL17910702M.json');
-        const a = this.http.get<any>('//openlibrary.org/search.json?q=the+lord+of+the+rings');
-        console.log(a);
-     //return this.http.get<any>('//openlibrary.org/books/OL17910702M.json');
-     return this.http.get<any>('//openlibrary.org/search.json?q=' + name);
-      }
+  getBooksDetails(name: string): Observable<any> {
+    return this.http.get<any>('//openlibrary.org/search.json?q=' + name);
+  }
 }
